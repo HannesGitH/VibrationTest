1a9640e02e216ad256da0df34e1a466fc85230f4
HannesH_Dev <33062605+HannesGitH@users.noreply.github.com>
2021-02-22 12:58:42 +0100

    refactored names

--- app/src/main/java/hannepps/tools/vibrationtest/MainActivity.java
+++ app/src/main/java/hannepps/tools/vibrationtest/MainActivity.java
@@ -20,125 +20,123 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import hannepps.tools.vibrationtest.R;
-
 import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView wlt;
-    TextView hnt;
-    SeekBar wl;
-    SeekBar hn;
+    TextView wavelength_textView;
+    TextView hardness_textView;
+    SeekBar wavelenght_slider;
+    SeekBar hardness_slider;
 
-    TextView wft;
-    VerticalSeekBar[] wfs;
-    MyLayout wfh;
-    TextView fct;
-    TextView pct;
-    SeekBar fc;
-    SeekBar pc;
+    TextView waveform_textView;
+    VerticalSeekBar[] waveform_seekbars;
+    MyLayout waveform_container;
+    TextView frequency_textView;
+    TextView precision_textView;
+    SeekBar frequency_slider;
+    SeekBar precision_slider;
 
-    Button fun;
-    ToggleButton mode;
-    Switch switvh;
-    boolean advanced=false;
+    Button startstop_button;
+    ToggleButton advanced_toggleButton;
+    Switch repeat_switch;
+    boolean isInAdvancedMode =false;
 
-    boolean vib=false;
+    boolean isCurrentlyVibrating =false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wl= findViewById(R.id.wl);
-        hn= findViewById(R.id.hn);
-        fun= findViewById(R.id.button);
-        switvh=findViewById(R.id.switch1);
-        mode= findViewById(R.id.toggleButton);
-        wlt= findViewById(R.id.wltext);
-        hnt= findViewById(R.id.hntext);
-        wft= findViewById(R.id.wftext);
-        wfh= findViewById(R.id.container);
-        pc= findViewById(R.id.pc);
-        pct= findViewById(R.id.pctext);
-        fc= findViewById(R.id.fc);
-        fct= findViewById(R.id.fctext);
+        wavelenght_slider = findViewById(R.id.wl);
+        hardness_slider = findViewById(R.id.hn);
+        startstop_button = findViewById(R.id.button);
+        repeat_switch =findViewById(R.id.switch1);
+        advanced_toggleButton = findViewById(R.id.toggleButton);
+        wavelength_textView = findViewById(R.id.wltext);
+        hardness_textView = findViewById(R.id.hntext);
+        waveform_textView = findViewById(R.id.wftext);
+        waveform_container = findViewById(R.id.container);
+        precision_slider = findViewById(R.id.pc);
+        precision_textView = findViewById(R.id.pctext);
+        frequency_slider = findViewById(R.id.fc);
+        frequency_textView = findViewById(R.id.fctext);
 
-        wfs=new VerticalSeekBar[1];
-        wfs[0]=findViewById(R.id.vertical_Seekbar);
+        waveform_seekbars =new VerticalSeekBar[1];
+        waveform_seekbars[0]=findViewById(R.id.vertical_Seekbar);
 
-        wl.setProgress(75);
-        hn.setProgress(50);
-        fc.setProgress(50);
+        wavelenght_slider.setProgress(75);
+        hardness_slider.setProgress(50);
+        frequency_slider.setProgress(50);
 
-        mode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        advanced_toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                advanced=b;
-                if(advanced){
-                    hn.setVisibility(View.GONE);
-                    wl.setVisibility(View.GONE);
-                    hnt.setVisibility(View.GONE);
-                    wlt.setVisibility(View.GONE);
+                isInAdvancedMode =b;
+                if(isInAdvancedMode){
+                    hardness_slider.setVisibility(View.GONE);
+                    wavelenght_slider.setVisibility(View.GONE);
+                    hardness_textView.setVisibility(View.GONE);
+                    wavelength_textView.setVisibility(View.GONE);
 
-                    wft.setVisibility(View.VISIBLE);
-                    wfh.setVisibility(View.VISIBLE);
-                    pc.setVisibility(View.VISIBLE);
-                    pct.setVisibility(View.VISIBLE);
-                    fc.setVisibility(View.VISIBLE);
-                    fct.setVisibility(View.VISIBLE);
+                    waveform_textView.setVisibility(View.VISIBLE);
+                    waveform_container.setVisibility(View.VISIBLE);
+                    precision_slider.setVisibility(View.VISIBLE);
+                    precision_textView.setVisibility(View.VISIBLE);
+                    frequency_slider.setVisibility(View.VISIBLE);
+                    frequency_textView.setVisibility(View.VISIBLE);
                     //for(VerticalSeekBar w:wfs){w.setVisibility(View.VISIBLE);}
 
                 }else{
-                    hn.setVisibility(View.VISIBLE);
-                    wl.setVisibility(View.VISIBLE);
-                    hnt.setVisibility(View.VISIBLE);
-                    wlt.setVisibility(View.VISIBLE);
+                    hardness_slider.setVisibility(View.VISIBLE);
+                    wavelenght_slider.setVisibility(View.VISIBLE);
+                    hardness_textView.setVisibility(View.VISIBLE);
+                    wavelength_textView.setVisibility(View.VISIBLE);
 
-                    wft.setVisibility(View.GONE);
-                    wfh.setVisibility(View.GONE);
-                    pc.setVisibility(View.GONE);
-                    pct.setVisibility(View.GONE);
-                    fc.setVisibility(View.GONE);
-                    fct.setVisibility(View.GONE);
+                    waveform_textView.setVisibility(View.GONE);
+                    waveform_container.setVisibility(View.GONE);
+                    precision_slider.setVisibility(View.GONE);
+                    precision_textView.setVisibility(View.GONE);
+                    frequency_slider.setVisibility(View.GONE);
+                    frequency_textView.setVisibility(View.GONE);
                     //for(VerticalSeekBar w:wfs){w.setVisibility(View.GONE);}
                 }
 
             }
         });
 
-        fun.setOnClickListener(new View.OnClickListener() {
+        startstop_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(!vib) {
+                if(!isCurrentlyVibrating) {
                     vib();
-                    fun.setText("Stop");
-                    vib=true;
+                    startstop_button.setText("Stop");
+                    isCurrentlyVibrating =true;
                 }else{
                     vibrate(new int[]{1,0},new long[]{10,10},false);
-                    fun.setText("Have Fun");
-                    vib=false;
+                    startstop_button.setText("Have Fun");
+                    isCurrentlyVibrating =false;
                 }
             }
         });
-        pc.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        precision_slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 setPrecision(progress);
             }
             public void onStartTrackingTouch(SeekBar seekBar) { }
             public void onStopTrackingTouch(SeekBar seekBar) { }
         });
-        fc.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        frequency_slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(vib){
+                if(isCurrentlyVibrating){
                     vib();
                 }
             }
             public void onStartTrackingTouch(SeekBar seekBar) { }
             public void onStopTrackingTouch(SeekBar seekBar) { }
         });
-        wl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        wavelenght_slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(vib){
+                if(isCurrentlyVibrating){
                     vib();
                 }
             }
@@ -150,19 +148,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void vib(){
-        boolean repeat=switvh.isChecked();
-        if(!advanced){
-            int hardness=hn.getProgress();
-            long wave=(wl.getProgress())^2+1;
+        boolean repeat= repeat_switch.isChecked();
+        if(!isInAdvancedMode){
+            int hardness= hardness_slider.getProgress();
+            long wave=(wavelenght_slider.getProgress())^2+1;
             vibrate(new int[]{hardness,0},new long[]{wave,wave},repeat);
         }else{
-            int len=wfs.length;
+            int len= waveform_seekbars.length;
             long[]wls=new long[len];
             int[] hns=new int[len];
-            long stepsize=((fc.getMax()-fc.getProgress())^3)*10/len+1;
+            long stepsize=((frequency_slider.getMax()- frequency_slider.getProgress())^3)*10/len+1;
             for(int i=0;i<len;i++){
                 wls[i]=stepsize;
-                hns[i]=wfs[i].getProgress();
+                hns[i]= waveform_seekbars[i].getProgress();
             }
             vibrate(hns,wls,repeat);
         }
@@ -185,47 +183,47 @@ public class MainActivity extends AppCompatActivity {
 
 
     void setPrecision(int p){
-        for(VerticalSeekBar w:wfs)
+        for(VerticalSeekBar w: waveform_seekbars)
         {
-            wfh.removeView(w);
+            waveform_container.removeView(w);
         }
-        wfs=new VerticalSeekBar[p];
+        waveform_seekbars =new VerticalSeekBar[p];
 
         for(int i=0;i<p;i++){
             VerticalSeekBar w=new VerticalSeekBar(this);
             w.setMax(255);
-            w.setLayoutParams(new ViewGroup.LayoutParams(wfh.getWidth()/p, ViewGroup.LayoutParams.MATCH_PARENT));
+            w.setLayoutParams(new ViewGroup.LayoutParams(waveform_container.getWidth()/p, ViewGroup.LayoutParams.MATCH_PARENT));
             w.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#009688")));
 
-            wfs[i]=w;
-            wfh.addView(w);
+            waveform_seekbars[i]=w;
+            waveform_container.addView(w);
         }
-        wfh.setOnTouchListener(new View.OnTouchListener(){
+        waveform_container.setOnTouchListener(new View.OnTouchListener(){
             @Override
             public boolean onTouch(View v, MotionEvent event){
                 if(event.getHistorySize()>0){
                     //got some skipped values
                     for(int j=0;j<event.getHistorySize();j++){
                         int x=(int)event.getHistoricalX(j);
-                        int width = wfh.getWidth() / wfh.getChildCount();
-                        int newp = (int) ((wfh.getBottom() - event.getHistoricalY(j)) * 260 / (wfh.getBottom() - wfh.getTop()) - 380);
+                        int width = waveform_container.getWidth() / waveform_container.getChildCount();
+                        int newp = (int) ((waveform_container.getBottom() - event.getHistoricalY(j)) * 260 / (waveform_container.getBottom() - waveform_container.getTop()) - 380);
                         newp = (newp >= 255) ? 255 : newp;
                         newp = (newp <= 0) ? 0 : newp;
                         System.out.println(newp);
                         try {
-                            ((VerticalSeekBar) wfh.getChildAt(x / width)).setProgress(newp);
+                            ((VerticalSeekBar) waveform_container.getChildAt(x / width)).setProgress(newp);
                         } catch (Exception e) {
                         }
                     }
                 }
                     int x = ((int) event.getX());
-                    int width = wfh.getWidth() / wfh.getChildCount();
-                    int newp = (int) ((wfh.getBottom() - event.getY()) * 260 / (wfh.getBottom() - wfh.getTop()) - 380);
+                    int width = waveform_container.getWidth() / waveform_container.getChildCount();
+                    int newp = (int) ((waveform_container.getBottom() - event.getY()) * 260 / (waveform_container.getBottom() - waveform_container.getTop()) - 380);
                     newp = (newp >= 255) ? 255 : newp;
                     newp = (newp <= 0) ? 0 : newp;
                     System.out.println(newp);
                     try {
-                        ((VerticalSeekBar) wfh.getChildAt(x / width)).setProgress(newp);
+                        ((VerticalSeekBar) waveform_container.getChildAt(x / width)).setProgress(newp);
                     } catch (Exception e) {
                     }
 
